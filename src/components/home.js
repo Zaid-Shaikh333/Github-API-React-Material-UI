256094ecaa4c59d071a8871c11d14bad3fad57c8
Zaid-Shaikh333 <mohdshaikh225@gmail.com>
2021-10-14 14:47:00 +0530

    GG

--- src/components/home.js
+++ src/components/home.js
@@ -3,7 +3,7 @@ import { Button, AppBar, Box, Toolbar } from '@mui/material';
 import { Repos } from './readRepos';
 import { Create } from './createRepo';
 //import { NotFound } from './NotFound';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import "../App.css";
 
 export const Home = () => {
