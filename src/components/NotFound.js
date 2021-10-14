cde2f65d45e25c70600e262c2bddfebfc38ad040
Zaid-Shaikh333 <mohdshaikh225@gmail.com>
2021-10-14 13:01:10 +0530

    Added 404

---
+++ src/components/NotFound.js
@@ -0,0 +1,7 @@
+export default NotFound = () => {
+    return(
+        <div>
+            <h1 className="text-center">404 Not Found</h1>
+        </div>
+    )
+}
--- src/components/home.js
+++ src/components/home.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Button, AppBar, Box, Toolbar } from '@mui/material';
 import { Repos } from './readRepos';
 import { Create } from './createRepo';
+import { NotFound } from './NotFound';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import "../App.css";
 
 export const Home = () => {
 
     return (
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
             <div className="mt-2 mx-2">
                 <Box sx={{ flexGrow: 1 }}>
                     <AppBar position="static">
@@ -41,7 +42,7 @@ export const Home = () => {
             </div>
             <Route path="/create" component={Create} exact />
             <Route path="/read" component={Repos} exact />
-            
+            <Route status={404} component={NotFound}/>
         </Router>
     )
 }
